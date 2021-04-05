354d03f13a2b335d4963fb8feda7410533f9ea58
Thomas Gale <gale.thomasjames@gmail.com>
2021-04-05 11:13:35 +0000

    Reenabling the cmake build chain.

--- scripts/build-engine.sh
+++ scripts/build-engine.sh
@@ -2,22 +2,21 @@
 set -e
 
 # Build files
-# mkdir -p build-engine
-# cd build-engine
-# emcmake cmake ../src-engine/
-# make
-# cd ..
-
-# Build files, non cmake
 mkdir -p build-engine
 cd build-engine
+emcmake cmake ../src-engine/
+make
+cd ..
+
+# Build files, non cmake
+# mkdir -p build-engine
+# cd build-engine
 # em++ ../src-engine/test.cpp -o test.js -s WASM=0 -s ENVIRONMENT=web -s EXTRA_EXPORTED_RUNTIME_METHODS='["cwrap"]' -s MODULARIZE=1
 # emcc ../src-engine/test.cpp -o test.js --bind -s WASM=1 -s MODULARIZE=-s WASM=1 -s MODULARIZE=1
 # em++ ../src-engine/test.cpp ../src-engine/fib.cpp -o test.js -s WASM=1 -s EXPORT_ALL=1 || exit 1
 # em++ ../src-engine/test.cpp ../src-engine/fib.cpp -o test.js -s WASM=1 -s EXPORT_ALL=1 --bind || exit 1
-em++ ../src-engine/engine.cpp -o engine.js -s WASM=1 -s ENVIRONMENT=web -s MODULARIZE=1 --bind || exit 1
-
-cd ..
+# em++ ../src-engine/engine.cpp -o engine.js -s WASM=1 -s ENVIRONMENT=web -s MODULARIZE=1 --bind || exit 1
+# cd ..
 
 # Copy built files to CRA binding point
 mkdir -p src/engine
