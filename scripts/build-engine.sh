031ebd49d8b1913f227096e523d357e976d1e867
Thomas Gale <gale.thomasjames@gmail.com>
2021-04-05 10:39:31 +0000

    Calling wasm code from react :)

--- scripts/build-engine.sh
+++ scripts/build-engine.sh
@@ -15,14 +15,17 @@ cd build-engine
 # emcc ../src-engine/test.cpp -o test.js --bind -s WASM=1 -s MODULARIZE=-s WASM=1 -s MODULARIZE=1
 # em++ ../src-engine/test.cpp ../src-engine/fib.cpp -o test.js -s WASM=1 -s EXPORT_ALL=1 || exit 1
 # em++ ../src-engine/test.cpp ../src-engine/fib.cpp -o test.js -s WASM=1 -s EXPORT_ALL=1 --bind || exit 1
-em++ ../src-engine/fib.cpp -o test.js -s WASM=1 -s EXPORT_ALL=1 --bind || exit 1
+em++ ../src-engine/fib.cpp -o test.js -s WASM=1 -s EXPORT_ALL=1 -s ENVIRONMENT=web -s MODULARIZE=1 --bind || exit 1
+
 cd ..
 
 # Copy built files to CRA binding point
 mkdir -p src/engine
 cd build-engine 
+
 find . -name "*.wasm" -exec cp '{}' ../src/engine/ \;
 find . -name "*.js" -exec cp '{}' ../src/engine/ \;
+find ../src/engine/ -name "*.js" -exec sed -i '1i/* eslint-disable */' '{}' \;
 
 find . -name "*.wasm" -exec cp '{}' ../public/ \;
 find . -name "*.js" -exec cp '{}' ../public/ \;
--- src/App.tsx
+++ src/App.tsx
@@ -1,101 +1,34 @@
 import logo from "./logo.svg";
 import "./App.css";
-// import { pingIt } from "./index";
-// import { useWasm } from "react-wasm";
-// import { useEffect, useState } from "react";
-// import test from './engine/test.js';
-// import testwasm from './engine/test.wasm';
-// import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 function App() {
-  // useEffect(() => {
-  // const sample = test({
-  //   locateFile: () => {
-  //     return testwasm;
-  //   }
-  // });
+  const [fibIn, setFibIn] = useState(10);
 
-  // sample.then((core: any) => {
-  //   console.log("Lerp result: " + core.lerp(1.0, 2.0, 0.5))
-  // })
-
-  //   const path = process.env.PUBLIC_URL + "/engine/test.wasm";
-
-  //   const importObject = {
-  //     env: {
-  //       memoryBase: 0,
-  //       tableBase: 0,
-  //       memory: new WebAssembly.Memory({ initial: 256, maximum: 1024 }),
-  //       table: new WebAssembly.Table({ initial: 256, element: "anyfunc" }),
-  //       __assert_fail: function () {
-  //         // todo
-  //       },
-  //       emscripten_resize_heap: function () {
-  //         // todo
-  //       },
-  //       emscripten_memcpy_big: function () {
-  //         // todo
-  //       },
-  //       setTempRet0: function () {
-  //         // todo
-  //       },
-  //       _embind_register_function: function () {
-  //       },
-  //       _embind_register_void: function () {
-  //       },
-  //       _embind_register_bool: function () {
-  //       },
-  //       _embind_register_float: function () {
-  //       },
-  //       _embind_register_integer: function () {
-  //       },
-  //       _embind_register_std_string: function () {
-  //       },
-  //       _embind_register_std_wstring: function () {
-  //       },
-  //       _embind_register_emval: function () {
-  //       }
-  //     },
-  //   };
-
-  //   console.log(importObject)
-
-  //   WebAssembly.instantiateStreaming(fetch(path), importObject).then((obj) => {
-  //     console.log(obj)
-  //     // do stuff
-  //   });
-  // });
-
-  // useEffect(() => {
-  //   let engine = require("./engine/test.js");
-  //   engine().then((e: any) => {
-  //     console.log(e)
-  //   });
-  // });
-
-  // const [ wasmStatus, setWasmStatus ] = useState('');
-
-  // const { loading, error, data } = useWasm({
-  //   url: "/test.wasm",
-  // });
-
-  // // if (loading) setWasmStatus("Loading...");
-  // // if (error) setWasmStatus("An error has occurred");
-
-  // const [ result, setResult ] = useState(0);
-
-  // useEffect(() => {
-  //   if (data != null) {
-  //     setResult(data.instance.exports.int_sqrt(16));
-  //   }
-  // }, [data]);
+  const [fibOut, setFibOut] = useState(0);
+  useEffect(() => {
+    var factory = require("./engine/test");
+    factory().then((i: any) => {
+      var val = i.fib(fibIn);
+      console.log("react fib: " + val);
+      setFibOut(val);
+    });
+  }, [fibIn, setFibOut]);
 
   return (
     <div className="App">
       <header className="App-header">
-        {/* <div>Wasm Status: { wasmStatus }</div> */}
-        {/* <div>root 16 = { result }</div> */}
         <img src={logo} className="App-logo" alt="logo" />
+        <p>wasm fib input (capped to 40): {fibIn}</p>
+        <input
+          onChange={(val) => {
+            let numVal = Number(val.target.value);
+            if (numVal && numVal <= 40) {
+              setFibIn(numVal);
+            }
+          }}
+        />
+        <p>wasm fib output: {fibOut}</p>
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
