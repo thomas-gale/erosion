33ad929512ca8113a39819f7591a2bef03f0d093
Thomas Gale <gale.thomasjames@gmail.com>
2021-04-05 09:51:32 +0000

    Only build the fib test function.

--- scripts/build-engine.sh
+++ scripts/build-engine.sh
@@ -14,7 +14,8 @@ cd build-engine
 # em++ ../src-engine/test.cpp -o test.js -s WASM=0 -s ENVIRONMENT=web -s EXTRA_EXPORTED_RUNTIME_METHODS='["cwrap"]' -s MODULARIZE=1
 # emcc ../src-engine/test.cpp -o test.js --bind -s WASM=1 -s MODULARIZE=-s WASM=1 -s MODULARIZE=1
 # em++ ../src-engine/test.cpp ../src-engine/fib.cpp -o test.js -s WASM=1 -s EXPORT_ALL=1 || exit 1
-em++ ../src-engine/test.cpp ../src-engine/fib.cpp -o test.js -s WASM=1 -s EXPORT_ALL=1 --bind || exit 1
+# em++ ../src-engine/test.cpp ../src-engine/fib.cpp -o test.js -s WASM=1 -s EXPORT_ALL=1 --bind || exit 1
+em++ ../src-engine/fib.cpp -o test.js -s WASM=1 -s EXPORT_ALL=1 --bind || exit 1
 cd ..
 
 # Copy built files to CRA binding point
