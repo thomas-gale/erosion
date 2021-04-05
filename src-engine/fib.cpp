0deb56f1162670af110596263d47d5becfa1bea8
Thomas Gale <gale.thomasjames@gmail.com>
2021-04-05 09:27:59 +0000

    Exporting the binding and calling it from js in html script tagged section.

--- src-engine/fib.cpp
+++ src-engine/fib.cpp
@@ -1,4 +1,5 @@
 #include "fib.hpp"
+#include <emscripten/bind.h>
 
 int fib(int x) {
   if (x < 1)
@@ -6,4 +7,8 @@ int fib(int x) {
   if (x == 1)
     return 1;
   return fib(x - 1) + fib(x - 2);
+}
+
+EMSCRIPTEN_BINDINGS(my_module) {
+    emscripten::function("fib", &fib);
 }
