56223fc0a513cc64fa661cdda43d80124377c1ba
Thomas Gale <gale.thomasjames@gmail.com>
2021-04-09 16:33:31 +0000

    WIP: Magnum cmake building.

--- scripts/build-engine.sh
+++ scripts/build-engine.sh
@@ -6,7 +6,9 @@ cd engine
 mkdir -p build && cd build
 cmake .. \
     -DCMAKE_TOOLCHAIN_FILE=../toolchains/generic/Emscripten-wasm.cmake \
-    #-DCMAKE_PREFIX_PATH=/home/vscode/emsdk/upstream/emscripten/system \
-    -DCMAKE_INSTALL_PREFIX=../../public/magnum
+    -DCMAKE_PREFIX_PATH=/home/vscode/emsdk/upstream/emscripten/system \
+    -DCMAKE_INSTALL_PREFIX=../../public/magnum \
+    -DCORRADE_RC_EXECUTABLE=../lib/corrade/src
+
 cmake --build .
 cmake --build . --target install
--- scripts/clean-engine.sh
+++ scripts/clean-engine.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-sudo rm -r engine/build/*
+sudo rm -r engine/build/
