7eef222cd74634d0e2f952de7e0cca51da2332ff
Thomas Gale <gale.thomasjames@gmail.com>
2021-04-09 16:44:53 +0000

    Tidied scripts, removed unneeded flags.

--- scripts/build-engine.sh
+++ scripts/build-engine.sh
@@ -6,7 +6,7 @@ cd engine
 mkdir -p build && cd build
 cmake .. \
     -DCMAKE_TOOLCHAIN_FILE=../toolchains/generic/Emscripten-wasm.cmake \
-    -DCMAKE_PREFIX_PATH=/home/vscode/emsdk/upstream/emscripten/system \
+    -DCMAKE_PREFIX_PATH=~/emsdk/upstream/emscripten/system \
     -DCMAKE_INSTALL_PREFIX=../../public/magnum \
     -DCORRADE_RC_EXECUTABLE=../lib/corrade/src
 
--- scripts/clean-engine.sh
+++ scripts/clean-engine.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
 sudo rm -r engine/build/
+sudo rm -r public/magnum/
