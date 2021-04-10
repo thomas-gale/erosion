9e18e010050f4ed743ed58e9fb574d0f3c551fa6
Thomas Gale <gale.thomasjames@gmail.com>
2021-04-10 18:28:16 +0000

    Storing the corrade-rc binary locally in the repo.

--- scripts/build-corrade-rc.sh
+++ scripts/build-corrade-rc.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 set -e
 
+# output
+mkdir -p engine/corrade-rc
+INSTALL_PRE=$(pwd)/engine/corrade-rc
+
+# build
 cd engine/lib/corrade
 mkdir -p build
 cd build
-cmake -DCMAKE_INSTALL_PREFIX=/usr ..
+cmake -DCMAKE_INSTALL_PREFIX=$INSTALL_PRE ..
 make
 sudo make install # sudo may be needed
--- scripts/build-engine.sh
+++ scripts/build-engine.sh
@@ -12,7 +12,7 @@ cmake .. \
     -DCMAKE_TOOLCHAIN_FILE=../toolchains/generic/Emscripten-wasm.cmake \
     -DCMAKE_PREFIX_PATH=~/emsdk/upstream/emscripten/system \
     -DCMAKE_INSTALL_PREFIX=../../public/magnum \
-    -DCORRADE_RC_EXECUTABLE=/usr/bin/corrade-rc
+    -DCORRADE_RC_EXECUTABLE=../corrade-rc/bin/corrade-rc
 
 cmake --build .
 cmake --build . --target install
