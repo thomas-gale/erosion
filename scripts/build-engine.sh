683c86f19b500ef6c8161b2fc3ff75803bb37a54
Thomas Gale <gale.thomasjames@gmail.com>
2021-04-17 09:11:05 +0000

    Found correct flag to force OpenGL ES 3
    set(TARGET_GLES2 OFF CACHE BOOL "" FORCE)
    
    This means I can switch back to unhacked magnum v2020.06 tag.

--- scripts/build-engine.sh
+++ scripts/build-engine.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 set -e
 
+# If the first argument is non-empty, this script will build a local sdl2 application
+# rather than a webgl2 application
+
 # generate taichi code
 sudo docker build ./engine/lib/taichi.js -t taichihub
 python3.8 ./scripts/taichi-c-generator.py
@@ -8,10 +11,12 @@ python3.8 ./scripts/taichi-c-generator.py
 # build main magnum emscripten application.
 cd engine 
 mkdir -p build && cd build
+mkdir -p install
 cmake .. \
+    -DLOCALTEST=$1 \
     -DCMAKE_TOOLCHAIN_FILE=../toolchains/generic/Emscripten-wasm.cmake \
     -DCMAKE_PREFIX_PATH=~/emsdk/upstream/emscripten/system \
-    -DCMAKE_INSTALL_PREFIX=../../public/magnum \
+    -DCMAKE_INSTALL_PREFIX=./install \
     -DCORRADE_RC_EXECUTABLE=../corrade-rc/bin/corrade-rc
 
 cmake --build .
@@ -20,7 +25,6 @@ cd ../..
 
 # copy built files to cra binding point
 mkdir -p src/engine
-find ./public/magnum/ -name "*.wasm" -exec cp '{}' ./public/ \;
-find ./public/magnum/ -name "engine.js" -exec cp '{}' ./src/engine/ \;
+find ./engine/build/install/ -name "*.wasm" -exec cp '{}' ./public/ \;
+find ./engine/build/install/ -name "engine.js" -exec cp '{}' ./src/engine/ \;
 find ./src/engine/ -name "engine.js" -exec sed -i '1i/* eslint-disable */' '{}' \;
-rm -r ./public/magnum
