7e5faf0c188c6f987bec68331b3324392ee658b9
Thomas Gale <gale.thomasjames@gmail.com>
2021-04-24 18:00:44 +0000

    Adding gravity x slider to main menu.

--- src/views/surfaces/MainMenuDrawer.tsx
+++ src/views/surfaces/MainMenuDrawer.tsx
@@ -1,5 +1,22 @@
-import { Drawer } from '@material-ui/core';
-import React from 'react';
+import {
+  Divider,
+  Drawer,
+  Grid,
+  List,
+  ListItem,
+  ListItemIcon,
+  ListItemText,
+  makeStyles,
+  Slider,
+} from '@material-ui/core';
+import React, { useState } from 'react';
+import VerticalAlignBottomIcon from '@material-ui/icons/VerticalAlignBottom';
+
+const useStyles = makeStyles({
+  list: {
+    width: 250,
+  },
+});
 
 export interface MainMenuDrawerProps {
   open: boolean;
@@ -7,10 +24,43 @@ export interface MainMenuDrawerProps {
 }
 
 export const MainMenuDrawer = (props: MainMenuDrawerProps): JSX.Element => {
+  const classes = useStyles();
   const { open, onClose } = props;
+
+  const [gravityX, setGravityX] = useState<number>(4);
+
   return (
     <Drawer anchor="left" open={open} onClose={onClose}>
-      <div>Hello Drawer World</div>
+      <div className={classes.list}>
+        <List>
+          <ListItem key={'gravity-title'}>
+            <ListItemIcon>
+              <VerticalAlignBottomIcon />
+            </ListItemIcon>
+            <ListItemText primary={'gravity'} />
+          </ListItem>
+          <ListItem key={'gravity-x'}>
+            <Grid container spacing={4}>
+              <Grid item>
+                <ListItemText primary={'x'} />
+              </Grid>
+              <Grid item xs>
+                <Slider
+                  min={-10.0}
+                  max={10.0}
+                  step={0.5}
+                  value={gravityX}
+                  onChange={(event: unknown, newValue: number | number[]) =>
+                    setGravityX(newValue as number)
+                  }
+                  aria-labelledby="continuous-slider"
+                />
+              </Grid>
+            </Grid>
+          </ListItem>
+        </List>
+        <Divider />
+      </div>
     </Drawer>
   );
 };
