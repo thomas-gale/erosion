229333dd4c9657843d7edeb2a634a2f34c6eb51d
Thomas Gale <gale.thomasjames@gmail.com>
2021-10-19 22:13:03 +0000

    Migrating App.tsx away for jss.

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,70 +1,50 @@
-import { Engine, EngineCanvas } from '../engine/Engine';
-import { Backdrop, Box, CircularProgress, Grid } from '@material-ui/core';
-import React, { useState } from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import { TopNav } from './surfaces/TopNav';
-import { MainMenuDrawer } from './surfaces/MainMenuDrawer';
-
-const useStyles = makeStyles((theme) => ({
-  '@global': {
-    '*::-webkit-scrollbar': {
-      width: '0.4em',
-    },
-    '*::-webkit-scrollbar-track': {
-      '-webkit-box-shadow': 'inset 0 0 6px rgba(0,0,0,0.00)',
-    },
-    '*::-webkit-scrollbar-thumb': {
-      backgroundColor: 'rgba(0,0,0,.1)',
-      outline: '1px solid slategrey',
-    },
-  },
-  fullScreen: {
-    margin: 0,
-    height: '100vh',
-    width: '100vw',
-  },
-  backdrop: {
-    zIndex: theme.zIndex.drawer + 1,
-    color: '#fff',
-    pointerEvents: 'auto',
-  },
-  uiOverlay: {
-    position: 'fixed' /* Sit on top of the page content */,
-    top: 0,
-    left: 0,
-    right: 0,
-    pointerEvents: 'none',
-    height: '100vh',
-    maxHeight: '100vh',
-  },
-  uiPrimaryGridContainer: {
-    justify: 'flex-start',
-  },
-  uiPrimaryGridElement: {
-    pointerEvents: 'auto',
-  },
-}));
+import { Engine, EngineCanvas } from "../engine/Engine";
+import { Backdrop, Box, CircularProgress, Grid } from "@material-ui/core";
+import React, { useState } from "react";
+import { TopNav } from "./surfaces/TopNav";
+import { MainMenuDrawer } from "./surfaces/MainMenuDrawer";
 
 const App = (): JSX.Element => {
-  const classes = useStyles();
-
   const [engine, setEngine] = useState<undefined | Engine>(undefined);
   const [loading, setLoading] = useState(false);
   const [mainMenuOpen, setMainMenuOpen] = useState(false);
 
   return (
-    <Box className={classes.fullScreen}>
+    <Box
+      style={{
+        margin: 0,
+        height: "100vh",
+        width: "100vw",
+      }}
+    >
       <EngineCanvas
         setEngine={setEngine}
         setLoaded={(loaded: boolean) => setLoading(!loaded)}
       />
-      <Backdrop className={classes.backdrop} open={loading}>
+      <Backdrop
+        style={{
+          // zIndex: 'zIndex.drawer' + 1,
+          color: "#fff",
+          pointerEvents: "auto",
+        }}
+        open={loading}
+      >
         <CircularProgress color="inherit" />
       </Backdrop>
-      <Box className={classes.uiOverlay}>
-        <Grid container className={classes.uiPrimaryGridContainer}>
+      <Box
+        style={{
+          position: "fixed",
+          top: 0,
+          left: 0,
+          right: 0,
+          pointerEvents: "none",
+          height: "100vh",
+          maxHeight: "100vh",
+        }}
+      >
+        <Grid container style={{ justifyContent: "flex-start" }}>
           <Grid item xs={12}>
-            <div className={classes.uiPrimaryGridElement}>
+            <div style={{ pointerEvents: "auto" }}>
               <TopNav menuClicked={() => setMainMenuOpen(!mainMenuOpen)} />
             </div>
           </Grid>
