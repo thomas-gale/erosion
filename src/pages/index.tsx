146bcfc4e46de067bf67c0a5bbc73d3e9018cb3f
Thomas Gale <gale.thomasjames@gmail.com>
2021-10-19 21:55:13 +0000

    Adding mui for next.js reference implementation.

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,16 +1,5 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from '../components/App';
-import CssBaseline from '@material-ui/core/CssBaseline';
-import { ThemeProvider } from '@material-ui/core/styles';
-import { theme } from '../env/theme';
+import App from "../components/App";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <ThemeProvider theme={theme}>
-      <CssBaseline />
-      <App />
-    </ThemeProvider>
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+export default function Index() {
+  return <App />;
+}
