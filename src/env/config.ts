588f2e767b841740cdb5e2be760cc34efd13c3a2
Thomas Gale <gale.thomasjames@gmail.com>
2021-10-22 13:24:12 +0000

    Futher removed things related to the wasm engine build.
    Fixing package.json dev script.

--- src/env/config.ts
+++ src/env/config.ts
@@ -1,12 +1,6 @@
 export const config = {
   topNav: {
-    name: 'erosion',
-    gitHubURL: 'https://github.com/thomas-gale/erosion',
-  },
-  engine: {
-    gravity: {
-      defaultX: 0,
-      defaultY: -9.81,
-    },
+    name: "erosion",
+    gitHubURL: "https://github.com/thomas-gale/erosion",
   },
 };
