3a9bca36521ed9d6b2b05834a848d5dad2bb8bbc
Thomas Gale <gale.thomasjames@gmail.com>
2022-02-27 10:12:53 +0000

    Quick tidy.

--- apps/web/src/components/viewport/terrain/Chunk.tsx
+++ apps/web/src/components/viewport/terrain/Chunk.tsx
@@ -1,6 +1,5 @@
-import { useEffect, useRef, useState } from "react";
+import { useEffect, useState } from "react";
 import {
-  InitResponse,
   LoadMeshResponse,
   TerrainInputData,
   TerrainPostMessageEvent,
@@ -91,87 +90,9 @@ export const Chunk = ({
     );
   }, [terrainWorker, xMax, xMin, zMax, zMin]);
 
-  // Init (if needed) and request mesh update from the web worker
-  // useEffect(() => {
-  //   (async () => {
-  //     if (!terrainWorker.current) {
-  //       console.log(
-  //         `Creating and initializing web worker for ${xMin}, ${zMin}...`
-  //       );
-  //       terrainWorker.current = new Worker(
-  //         new URL("../../../engine/terrain.worker", import.meta.url)
-  //       );
-
-  //       // Configure the callbacks
-  //       terrainWorker.current.onmessage = async (
-  //         evt: TerrainPostMessageEvent
-  //       ) => {
-  //         if (evt.data.type === "init") {
-  //           const resp = evt.data.payload as InitResponse;
-  //           // Now trigger the loadMesh
-  //           if (resp) {
-  //             console.log(`Triggering web worker mesh for ${xMin}, ${zMin}...`);
-  //             await terrainWorker.current.postMessage({
-  //               type: "loadMesh",
-  //               payload: {
-  //                 xMin,
-  //                 zMin,
-  //                 xMax,
-  //                 zMax,
-  //               },
-  //             } as TerrainInputData);
-  //             console.log(`Triggered web worker mesh for ${xMin}, ${zMin}!`);
-  //           } else {
-  //             console.warn(
-  //               `WebWorker init response for ${xMin}, ${zMin}`,
-  //               resp
-  //             );
-  //           }
-  //         } else if (evt.data.type === "loadMesh") {
-  //           const resp = evt.data.payload as LoadMeshResponse;
-  //           console.log(`Loading terrain mesh for ${xMin}, ${zMin}...`);
-
-  //           // DEBUG
-  //           const verts = resp.positions.flat();
-  //           const cells = resp.cells.flat();
-
-  //           console.log("number verts", verts.length);
-  //           console.log("number cells", cells.length);
-
-  //           console.log("expanding verts", upperPow2(verts.length));
-  //           console.log("expanding cells", upperPow2(cells.length));
-
-  //           // Expand and contract the verts (update the maxVerts/maxCells)
-  //           // const verts = new Float32Array(resp.positions.flat());
-
-  //           setVerts(new Float32Array(resp.positions.flat()));
-  //           setCells(new Uint32Array(resp.cells.flat()));
-  //           setNumVerts(verts.length);
-  //           setNumCells(cells.length);
-  //           console.log(`Loaded terrain mesh for ${xMin}, ${zMin}!`);
-  //         }
-  //       };
-
-  //       console.log(`Created and initialized web worker for ${xMin}, ${zMin}!`);
-
-  //       console.log(`Triggering web worker init for ${xMin}, ${zMin} ...`);
-  //       await terrainWorker.current.postMessage({
-  //         type: "init",
-  //         payload: { seed },
-  //       } as TerrainInputData);
-  //       console.log(`Triggered web worker init for ${xMin}, ${zMin}!`);
-  //     }
-  //   })();
-  //   return () => {
-  //     console.log(`Terminating web worker for ${xMin}, ${zMin}...`);
-  //     terrainWorker.current.terminate();
-  //     terrainWorker.current = null;
-  //   };
-  // }, [seed, xMax, xMin, zMax, zMin]);
-
   return (
     <ChunkGeometry
-      key={`${verts?.length ?? 0}-${cells?.length ?? 0}`} // Trigger a re-render if the verts/cells Arrays change (buffer geometry requires this)
+      key={`${verts?.length ?? 0}-${cells?.length ?? 0}`} // Trigger a re-render if the verts/cells Array length change (buffer geometry requires this)
       cells={cells}
       numCells={numCells}
       verts={verts}
--- apps/web/src/components/viewport/terrain/Terrain.tsx
+++ apps/web/src/components/viewport/terrain/Terrain.tsx
@@ -1,6 +1,6 @@
 import { Chunk } from "./Chunk";
 import { config } from "../../../env/config";
-import { useEffect, useMemo, useState } from "react";
+import { useEffect, useState } from "react";
 import {
   InitResponse,
   TerrainInputData,
@@ -17,40 +17,6 @@ export interface TerrainProps {
 export const Terrain = ({
   nearestChunk: { x, z },
 }: TerrainProps): JSX.Element => {
-  /* 
-  Notes.
-  Move the terrain engine worker back here?
-  Or keep the terrain engine workers in the chunks?
-
-  create a higher order filtered callback in the mapping function which will tell the chunks when to re-render (and will provide update mesh)
-  */
-
-  // const terrainWorker = useRef<Worker>();
-
-  // Load terrain worker and register callbacks
-
-  // Using the nearest chunk to the camera focus, we can determine the chunks to render (this approximation relies on the camera elevation having a finite limit (of 45 degrees) - the chunk region is a hardcoded heuristic.)
-  // TODO - change this to a spiral about the point: https://stackoverflow.com/questions/3706219/algorithm-for-iterating-over-an-outward-spiral-on-a-discrete-2d-grid-from-the-or
-  // const chunkCoordsToLoad = useMemo(() => {
-  //   console.log(x, z);
-  //   const chunkCoords: { x: number; z: number }[] = [];
-
-  //   // Naive approach for loading rings of chunks around the camera.
-  //   for (let r = 0; r <= config.chunksToLoadAroundCamera; r++) {
-  //     for (let i = x - r; i <= x + r; i++) {
-  //       for (let j = z - r; j <= z + r; j++) {
-  //         if (i == x - r || j == z - r || i == x + r || j == z + r) {
-  //           chunkCoords.push({
-  //             x: i * config.chunkSize,
-  //             z: j * config.chunkSize,
-  //           });
-  //         }
-  //       }
-  //     }
-  //   }
-  //   return chunkCoords;
-  // }, [x, z]);
-
   const [terrainWorker] = useState<Worker>(
     new Worker(new URL("../../../engine/terrain.worker", import.meta.url))
   );
@@ -60,7 +26,6 @@ export const Terrain = ({
   // Initialize the terrain worker
   useEffect(() => {
     if (!terrainWorkerInitialized) {
-      // Configure the callback
       terrainWorker.addEventListener(
         "message",
         async (event: TerrainPostMessageEvent) => {
@@ -97,8 +62,6 @@ export const Terrain = ({
     <>
       {terrainWorkerInitialized && (
         <Chunk
-          // key={`${x}-${z}`}
-          // seed={config.testSeed}
           terrainWorker={terrainWorker}
           xMin={(x - 2) * config.chunkSize}
           zMin={(z - 2) * config.chunkSize}
