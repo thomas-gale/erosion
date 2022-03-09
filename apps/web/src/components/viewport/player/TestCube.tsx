6f5eef12e0f99e535343fc7875f2b0d92f59eb1f
Thomas Gale <gale.thomasjames@gmail.com>
2022-03-09 23:04:28 +0000

    WIP: Experimenting with gltfjsx conversion and import. Currently a floating/non animated player digger object.
    Track instance objects not currently working.

--- apps/web/src/components/viewport/player/TestCube.tsx
+++
@@ -1,8 +0,0 @@
-export const TestCube = () => {
-  return (
-    <mesh position={[5, 0, 0.5]}>
-      <boxGeometry />
-      <meshStandardMaterial wireframe color={"orange"} />
-    </mesh>
-  );
-};
