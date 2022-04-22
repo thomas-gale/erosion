0946140cff9ace6f212f05b17f941ec1994ea421
Thomas Gale <gale.thomasjames@gmail.com>
2022-04-22 21:16:58 +0100

    Digger bounding box corrected.

--- apps/web/src/components/viewport/player/Digger.tsx
+++ apps/web/src/components/viewport/player/Digger.tsx
@@ -1,19 +1,22 @@
 import { useRef } from "react";
-import type { Vector3 } from "three";
-import { Group } from "three";
+import type { Mesh } from "three";
 import { useBox, BoxProps } from "@react-three/cannon";
 import DiggerModel from "./Digger_v1_static";
 
 export const Digger = ({ position, ...props }: BoxProps) => {
   const [ref] = useBox(
     () => ({
-      arg: [15, 15, 15],
+      args: [2.5, 2.5, 5],
       mass: 10,
       position,
       ...props,
     }),
-    useRef<Group>(null)
+    useRef<Mesh>(null)
   );
 
-  return <DiggerModel ref={ref} />;
+  return (
+    <mesh ref={ref} dispose={null}>
+      <DiggerModel position={[0, -1, 0.5]} />
+    </mesh>
+  );
 };
