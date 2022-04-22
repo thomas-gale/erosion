4c9afac57e39e67828d95bd9309cb15ab21f8b5d
Thomas Gale <gale.thomasjames@gmail.com>
2022-04-22 21:02:36 +0100

    WIP: Experimenting with forward ref for digger group.

--- apps/web/src/components/viewport/player/Digger.tsx
+++ apps/web/src/components/viewport/player/Digger.tsx
@@ -1,5 +1,19 @@
+import { useRef } from "react";
+import type { Vector3 } from "three";
+import { Group } from "three";
+import { useBox, BoxProps } from "@react-three/cannon";
 import DiggerModel from "./Digger_v1_static";
 
-export const Digger = ({ ...props }: JSX.IntrinsicElements["group"]) => {
-  return <DiggerModel {...props} />;
+export const Digger = ({ position, ...props }: BoxProps) => {
+  const [ref] = useBox(
+    () => ({
+      arg: [15, 15, 15],
+      mass: 10,
+      position,
+      ...props,
+    }),
+    useRef<Group>(null)
+  );
+
+  return <DiggerModel ref={ref} />;
 };
